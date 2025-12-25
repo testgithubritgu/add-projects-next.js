c506ca028754d5744405814c77518d8f1d68f153
testgithubritgu <swapnilraut1698@gmail.com>
2025-12-25 15:37:47 +0530

    Refactors SectionHeader, ProductCard, and StatsCard components for consistent styling and improved UI elements

--- components/landing-page/products/product-card.jsx
+++ components/landing-page/products/product-card.jsx
@@ -1,11 +1,74 @@
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
+import { ChevronDownIcon, ChevronUpIcon, StarIcon } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
+  const  hasVoted = false
   return (
-    <div>
-      we
-    </div>
-  )
+    <Link href={`/products/${product.id}`}>
+      <Card
+        className={
+          "group card-hover hover:bg-primary-foreground/10 border-solid border-gray-400 min-h-[180px]"
+        }
+      >
+        <CardHeader className={"flex-1"}>
+          <div className="flex items-start gap-4">
+            <div className="flex-1 min-w-0">
+              <div className="flex items-start gap-2">
+                <CardTitle
+                  className={
+                    "text-lg group-hover:text-primary transition-colors"
+                  }
+                >
+                  {product.name}
+                </CardTitle>
+                {product.isFeatured && (
+                  <Badge
+                    className={"gap-1 bg-primary text-primary-foreground "}
+                  >
+                    <StarIcon className="size-3 fill-current" /> Featured
+                  </Badge>
+                )}
+              </div>
+              <CardDescription>{product.description}</CardDescription>
+            </div>
+            <div className="flex flex-col items-center gap-1 shrink-0 ">
+              <Button
+                variant="ghost"
+                size="icon-sm"
+                className={cn("h-8 w-8 text-primary hover:text-destructive",hasVoted ? "bg-primary/10 text-primary hover:bg-primary/20": "hover:bg-primary/10 hover:text-primary")}
+              >
+                <ChevronUpIcon className="size-5" />
+              </Button>
+              <span className="text-sm text-foreground transition-colors font-semibold">
+                10
+              </span>
+              <Button
+                variant="ghost"
+                size="icon-sm"
+                className={cn("h-8 w-8 text-primary hover:text-destructive",hasVoted ? "hover:text-destructive": "opacity-50 cursor-not-allowed")}
+              >
+                <ChevronDownIcon className="size-5" />
+              </Button>
+            </div>
+          </div>
+        </CardHeader>
+        <CardFooter>
+          <div className="flex gap-2 items-center ">
+            {product.tags.map((tag) => (
+              <Badge variant={"secondary"} key={tag}>
+                {tag}
+              </Badge>
+            ))}
+          </div>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
 }
 
 export default ProductCard
--- components/landing-page/stats-card.jsx
+++ components/landing-page/stats-card.jsx
@@ -5,7 +5,7 @@ const StatsCard = ({icon:Icon,label,value,hasBorder}) => {
   return (
     <div className={cn("space-y-2", hasBorder && "border-x border-border/50")}>
       <div className="flex items-center justify-center gap-2">
-        <Icon className="size-5 text-primary/70" />
+        <Icon className="size-5 text-pink-500" />
         <p className="text-3xl sm:text-4xl font-bold">{value}</p>
       </div>
       <p className="text-sm text-muted-foreground">{label}</p>
