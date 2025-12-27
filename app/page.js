21514e339d5312d9a4293bc94bba7a395f88482a
testgithubritgu <swapnilraut1698@gmail.com>
2025-12-27 20:57:46 +0530

    feat:added loader ui for while getting data from backend

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import FeaturedProducts from "@/components/landing-page/featured-section";
 import HeroSection from "@/components/landing-page/hero-section";
 import RecentlyLaunchedProducts from "@/components/landing-page/recently-launched-products";
+import { LoaderIcon } from "lucide-react";
 import { Suspense } from "react";
 
 
@@ -10,7 +11,10 @@ export default function Home() {
   
         <HeroSection />
         <FeaturedProducts />
-        <Suspense fallback="loading...">
+        <Suspense fallback={<div className="wrapper flex items-center gap-2 ">
+          Loading Recently launched products ...{" "}
+          <LoaderIcon  className="size-4 animate-spin"/>
+        </div>}>
 
         <RecentlyLaunchedProducts />
         </Suspense>
