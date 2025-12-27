c01155a1e5e3b86c8cb3c00ba6e1b5b65988e776
testgithubritgu <swapnilraut1698@gmail.com>
2025-12-27 10:41:04 +0530

    test

--- app/page.js
+++ app/page.js
@@ -10,7 +10,10 @@ export default function Home() {
   
         <HeroSection />
         <FeaturedProducts />
+        <Suspense fallback="loading...">
+
         <RecentlyLaunchedProducts />
+        </Suspense>
    
     </div>
   );
