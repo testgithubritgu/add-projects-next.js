9fc612d6fa4dee2bf6ad7b6cf9907b92174662de
testgithubritgu <swapnilraut1698@gmail.com>
2026-01-03 20:56:43 +0530

    implement product explore page with dynamic data

--- app/explore/page.jsx
+++ app/explore/page.jsx
@@ -1,14 +1,25 @@
+'use cache'
 import SectionHeader from "@/components/common/section-header";
 import { ProductExplore } from "@/components/products/product-explore";
+import { getAllProducts } from "@/lib/products/product-select";
 import { CompassIcon } from "lucide-react";
 
-export default function ExplorePage(){
+export default  async function ExplorePage(){
+    const products = await getAllProducts()
         return (
-            <div className="py-20">
-                    <div className="wrapper">
-                        <SectionHeader title={"Explore All Products"} icon={CompassIcon} description={"Brows and discover amazing project from our community"}/>
-                    <ProductExplore />
-                    </div>
+          <div className="py-20">
+            <div className="wrapper">
+              <div className="mb-12">
+                <SectionHeader
+                  title={"Explore All Products"}
+                  icon={CompassIcon}
+                  description={
+                    "Brows and discover amazing project from our community"
+                  }
+                />
+              </div>
+              <ProductExplore products={products}/>
             </div>
-        )
+          </div>
+        );
 } 
--- components/products/product-explore.jsx
+++ components/products/product-explore.jsx
@@ -1,38 +1,64 @@
-import { ClockIcon, TrendingUpIcon } from "lucide-react";
+'use client'
+import { ClockIcon, SearchIcon, TrendingUpIcon } from "lucide-react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import ProductCard from "./product-card";
+import {  useMemo, useState } from "react";
 
-export function ProductExplore (){
-    return (
-      <div>
-        <div className="flex flex-col sm:flex-row gap-4 mb-8 ">
+export  function ProductExplore({ products }) {
+    const [searchQuery,setSearchQuery] = useState("")
+  
 
-        <div className="flex-1 relative">
-          <Input />
-        </div>
+    const filteredProductsResult = useMemo(()=>{
+            if(searchQuery.length > 0){
+                return products.filter(prd  => prd.name.toLowerCase().includes(searchQuery.toLowerCase()))
+            
+            }
+            return products
         
+    },[searchQuery,products])
+   
+  return (
+    <div>
+      <div className="flex flex-col sm:flex-row gap-4 mb-8 ">
+        <div className="flex-1 relative">
+          <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground size-4" />
+
+          <Input
+            type={"text"}
+            placeholder="search products..."
+            className={"pl-10"}
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+          />
+        </div>
+
         <div className="flex gap-2 ">
           <Button variant="outline">
             <TrendingUpIcon />
-            Trending</Button>
+            Trending
+          </Button>
           <Button>
             <ClockIcon />
-            Recent</Button>
+            Recent
+          </Button>
         </div>
 
-        <div className="mb-6">
-        <p className="text-muted-foreground">Showing {products.length} Products</p>
-        </div>
-
-        <div className="grid-wrapper">
-            {
-                products.map((product)=>(
-                    <ProductCard key={product.id} product={product}/>
-                ))
-            }
-        </div>
-        </div>
+        <div className="mb-6"></div>
       </div>
-    );
+      <div className="mb-6">
+        <p className="text-muted-foreground">
+          Showing {filteredProductsResult.length} Products
+        </p>
+      </div>
+
+      <div className="grid-wrapper">
+        {filteredProductsResult.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
