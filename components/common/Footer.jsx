bdb56597c55008cc7c944ae5ab31ffbc8b3e773c
testgithubritgu <swapnilraut1698@gmail.com>
2026-01-03 21:45:13 +0530

    test

--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className='py-8 sm:py-10 lg:py-12 bg-muted/60'>
       <div className='wrapper px-4 sm:px-6 text-center sm:text-left'>
         <p className="text-sm sm:text-base text-muted-foreground">
-          &copy; {new Date().getFullYear()} i Built this. All rights reserved.
+          &copy; i Built this. All rights reserved.
         </p>
       </div>
     </footer>
