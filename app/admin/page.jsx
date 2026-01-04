a8079611a8af82b0ae09b04742f80b02afa9bdaf
testgithubritgu <swapnilraut1698@gmail.com>
2026-01-04 10:01:51 +0530

    build check

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,13 +1,5 @@
-import { auth, clerkClient } from "@clerk/nextjs/server"
-import { redirect } from "next/dist/server/api-utils"
 
 export default async function AdminPage(){
-    const {userId} = await auth()
-    if(!userId){
-        redirect("/sign-in")
-    }
-    const response = await clerkClient()
-    const user = await response.users.getUser(userId)
-    console.log(user)
+    
     return <div>Adming Page</div>
 }
